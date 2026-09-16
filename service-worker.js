56ada6ab0bbc05e43530d9d0d7a2c251cbe9b2a5
milovalcodes <milovalcodes@users.noreply.github.com>
2026-09-16 14:57:26 -0400

    Add character portraits and polish the interface

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'our-little-list-personalized-v7';
-const ASSETS = ['./','./index.html','./her.html','./him.html','./tasks.html','./reminders.html','./notes.html','./location.html','./styles.css','./shared.js','./tasks.js','./reminders.js','./notes.js','./location.js','./live-notes.js','./ui-helpers.js','./firebase-data.js','./firebase-config.js','./sun-moon-personalized.png','./manifest.webmanifest'];
+const CACHE = 'our-little-list-ui-v8';
+const ASSETS = ['./','./index.html','./her.html','./him.html','./tasks.html','./reminders.html','./notes.html','./location.html','./styles.css','./shared.js','./tasks.js','./reminders.js','./notes.js','./location.js','./live-notes.js','./ui-helpers.js','./firebase-data.js','./firebase-config.js','./sun-moon-personalized.png','./sun-profile.png','./moon-profile.png','./manifest.webmanifest'];
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
