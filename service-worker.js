10a73f161df65769a35b05b16769afd715781635
milovalcodes <milovalcodes@users.noreply.github.com>
2026-09-16 14:21:50 -0400

    Personalize the sun and moon artwork

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'our-little-list-sun-moon-v6';
-const ASSETS = ['./','./index.html','./her.html','./him.html','./tasks.html','./reminders.html','./notes.html','./location.html','./styles.css','./shared.js','./tasks.js','./reminders.js','./notes.js','./location.js','./live-notes.js','./ui-helpers.js','./firebase-data.js','./firebase-config.js','./icon.svg','./manifest.webmanifest'];
+const CACHE = 'our-little-list-personalized-v7';
+const ASSETS = ['./','./index.html','./her.html','./him.html','./tasks.html','./reminders.html','./notes.html','./location.html','./styles.css','./shared.js','./tasks.js','./reminders.js','./notes.js','./location.js','./live-notes.js','./ui-helpers.js','./firebase-data.js','./firebase-config.js','./sun-moon-personalized.png','./manifest.webmanifest'];
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
@@ -20,11 +20,10 @@ self.addEventListener('fetch', event => {
 self.addEventListener('push', event => {
   let data = { title:'Our Little List', body:'There’s a new little nudge waiting for you ♡', url:'./index.html' };
   try { data = { ...data, ...event.data.json() }; } catch (_) {}
-  event.waitUntil(self.registration.showNotification(data.title,{ body:data.body, icon:'./icon.svg', badge:'./icon.svg', data:{ url:data.url } }));
+  event.waitUntil(self.registration.showNotification(data.title,{ body:data.body, icon:'./sun-moon-personalized.png', badge:'./sun-moon-personalized.png', data:{ url:data.url } }));
 });
 
 self.addEventListener('notificationclick', event => {
   event.notification.close();
   event.waitUntil(clients.openWindow(event.notification.data?.url || './index.html'));
 });
-
