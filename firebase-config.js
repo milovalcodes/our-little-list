5ebdeb0c06e8a7f69e01d3336112a1112046fc1b
Emilio <66177427+milovalcodes@users.noreply.github.com>
2026-09-16 14:04:49 -0400

    Connect the website to the private shared space

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'REPLACE_WITH_FIREBASE_API_KEY',
-  authDomain: 'REPLACE_WITH_FIREBASE_AUTH_DOMAIN',
-  projectId: 'REPLACE_WITH_FIREBASE_PROJECT_ID',
-  storageBucket: 'REPLACE_WITH_FIREBASE_STORAGE_BUCKET',
-  messagingSenderId: 'REPLACE_WITH_FIREBASE_SENDER_ID',
-  appId: 'REPLACE_WITH_FIREBASE_APP_ID'
+  apiKey: 'AIzaSyAFUncHYGmf5j2O7DGm9PL53qPNsM8HkgE',
+  authDomain: 'our-little-list-5ea59.firebaseapp.com',
+  projectId: 'our-little-list-5ea59',
+  storageBucket: 'our-little-list-5ea59.firebasestorage.app',
+  messagingSenderId: '452546502505',
+  appId: '1:452546502505:web:7330c1e45c84f72172a7f8'
 };
+
